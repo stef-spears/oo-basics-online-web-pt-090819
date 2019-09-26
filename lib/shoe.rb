ece8bba8737e37283403347dfff512229b03ec96
Stef Spears <spears.sj87@gmail.com>
2019-09-26 03:38:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,15 @@
 class Shoe 
   
+  attr_accessor :color, :size, :material, :condition
+  
+  attr_reader :brand
+  
   def initialize(brand)
     @brand = brand
+  end 
+  
+  def cobble
+    puts 
   end 
   
 end 
