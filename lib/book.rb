3face8a86283ffcee4228520078b87f26fb71740
Stef Spears <spears.sj87@gmail.com>
2019-09-26 03:32:21 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -16,13 +16,24 @@ class Book
     @author 
   end 
   
+  def page_count=(page_count)
+    @page_count = page_count
+  end 
+  
   def page_count
     @page_count
+  end 
+  
+  def genre=(genre)
+    @genre = genre
   end 
   
   def genre 
     @genre 
   end
+  
+  def turn_page
+  end 
   
 end 
 
