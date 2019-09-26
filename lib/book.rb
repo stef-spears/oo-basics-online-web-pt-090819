200dc2244b4e66c02d003f8d561a3fc58005d20b
Stef Spears <spears.sj87@gmail.com>
2019-09-26 03:24:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,27 @@
 class Book
   
-  def initialize(title)
+  def initialize(title, author, page_count, genre)
     @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
   end
   
   def title 
     @title 
   end 
+  
+  def author
+    @author
+  end 
+  
+  def page_count
+    @page_count
+  end 
+  
+  def genre 
+    @genre 
+  end
   
 end 
 
